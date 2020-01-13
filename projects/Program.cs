1da487579bd7d8df0a8a4332f1f415fc38410291
Matt Lopez <matthew.lopez4011@gmail.com>
2020-01-13 00:01:38 -0600

    starter files

---
+++ projects/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace projects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("What is your name?");
+        }
+    }
+}
